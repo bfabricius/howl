40575bbbd124583433e154b664041970932015ef
Ben Fabricius <ben@htaudio.de>
2018-05-01 20:09:15 +0200

    Added heavy export config fields

--- scripts/howl/exports.sh
+++ scripts/howl/exports.sh
@@ -4,4 +4,5 @@ export PATH=/opt/howl-tlc/gcc-arm-none-eabi-7-2017-q4-major/bin:$PATH
 export TOOLROOT=/opt/howl-tlc/gcc-arm-none-eabi-7-2017-q4-major/bin/
 export FIRMWARESENDER=/usr/local/bin/firmwaresender
 export OWLDEVICE="OWL-MIDI MIDI 1"
-
+export HEAVYRELEASE=r2018.03
+export HEAVYTOKEN=
